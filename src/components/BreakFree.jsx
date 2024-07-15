d60131920c7d9e35ecd69e41f2108090ae37abb1
Red22K <ritwik22k@gmail.com>
2024-07-15 14:46:46 +0530

    Rotation to images

--- src/components/BreakFree.jsx
+++ src/components/BreakFree.jsx
@@ -15,19 +15,35 @@ function BreakFree() {
         <div className="sticky top-0 flex h-[screen] items-center overflow-hidden">
           <motion.div style={{x}} className="flex gap-4">
             <div className="h-screen relative font-extrabold bg-red-600 w-[100vw] text-[3em] md:text-[7em] text-white flex justify-center items-center">
-              <img src={fingers} className="absolute opacity-75" alt="" />
+              <img
+                src={fingers}
+                className="absolute opacity-75 rotate-45"
+                alt=""
+              />
               <div className="z-10">BREAK</div>
             </div>
             <div className="h-screen relative font-extrabold bg-white w-[100vw] text-[3em] md:text-[7em] text-red-600 flex justify-center items-center">
-              <img src={fingers} className="absolute opacity-25" alt="" />
+              <img
+                src={fingers}
+                className="absolute opacity-25 rotate-[15deg]"
+                alt=""
+              />
               <div className="z-10">FREE</div>
             </div>
             <div className="h-screen relative font-extrabold bg-red-600 w-[100vw] text-[3em] md:text-[7em] text-white flex justify-center items-center">
-              <img src={fingers} className="absolute opacity-75" alt="" />
+              <img
+                src={fingers}
+                className="absolute opacity-75 -rotate-45"
+                alt=""
+              />
               <div className="z-10">FROM</div>
             </div>
             <div className="h-screen relative font-extrabold bg-white w-[100vw] text-[3em] md:text-[7em] text-red-600 flex justify-center items-center">
-              <img src={fingers} className="absolute opacity-25" alt="" />
+              <img
+                src={fingers}
+                className="absolute opacity-25 rotate-180"
+                alt=""
+              />
               <div className="z-10">STRESS</div>
             </div>
           </motion.div>
